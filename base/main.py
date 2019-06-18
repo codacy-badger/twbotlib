88d7780bc32694b91a043f16c2a9dd8b80004ab0
Tom <25082829+truedl@users.noreply.github.com>
2019-06-18 20:53:16 +0300

    main.py (commented)

--- base/main.py
+++ base/main.py
@@ -14,6 +14,8 @@ class Bot:
         self.prefix = prefix
     
     def connect(self) -> bool:
+        """ Connection to the twitch IRC socket and returning boolean (On success is True and on fail is False). """
+
         try:
             self.sock.connect(('irc.twitch.tv', 6667))
             self.sock.send(f'PASS {self.auth.oauth_token}{self.__struct_end}'.encode('utf-8'))
@@ -24,8 +26,9 @@ class Bot:
             return False
     
     def wait_until_join(self) -> None:
-        not_completed = True
+        """ Wait until the bot is joined the chat. """
 
+        not_completed = True
         while not_completed:
             data = self.sock.recv(self.buffer).decode('utf-8').split('\n')
 
@@ -36,6 +39,8 @@ class Bot:
                     print(line)
 
     def send(self, message:str) -> bool:
+        """ Send message (string) to the connected chat and returning boolean (On success is True and on fail is False). """
+
         try:
             self.sock.send(f'PRIVMSG #{self.auth.channel_name} :{message}{self.__struct_end}'.encode('utf-8'))
             return True
@@ -43,12 +48,15 @@ class Bot:
             return False
 
     def read_commands(self, __globals:dict) -> None:
+        """ Read the exists commands from the file-globals. """
+
         for command in [x for x in __globals if '__command_' in x]:
             self.commands[command.replace('__command_', '')] = __globals[command]
     
     def run(self) -> None:
-        self.is_running = True
+        """ Run the bot and start the main while. """
 
+        self.is_running = True
         while self.is_running:
             data = self.sock.recv(self.buffer).decode('utf-8').split('\n')
 
@@ -64,12 +72,18 @@ class Bot:
                     print(line)
     
     def checkPrefix(self, __message:str) -> bool:
+        """ Check if message starts with the prefix. """
+
         return __message[:len(self.prefix)] == self.prefix
     
     def cutMessage(self, __message:str) -> str:
+        """ Cutting the prefix from the message. """
+
         return __message[len(self.prefix):]
 
     def getMessageObjectFromStr(self, __string:str) -> Message:
+        """ Returning Message object from string. """
+
         __string = __string.split('PRIVMSG #')[1]
         try:
             args = self.cutMessage(__string.split(' :')[1]).split(' ', 1)[1].replace('\r', '')
