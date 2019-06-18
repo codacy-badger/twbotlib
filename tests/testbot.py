2093aa9f9f7996b3bf31350c7738ba71c81df979
Tom <25082829+truedl@users.noreply.github.com>
2019-06-18 22:22:48 +0300

    testbot.py (updated)

--- tests/testbot.py
+++ tests/testbot.py
@@ -3,28 +3,34 @@ import twbotlib
 
 __auth = twbotlib.Auth(
     bot_username='bot_username',
-    oauth_token='oauth:token'
+    oauth_token='oauth:token',
+    channel_name='bot_username'
 )
 
 __bot = twbotlib.Bot(__auth)
 
-def __command_roll(message, args=None):
+async def startup():
+    await __bot.join()
+
+async def __command_roll(message, args=None):
     if args and int(args) < 101:
-        __bot.send(randint(1, int(args)))
+        await __bot.send(randint(1, int(args)))
     else:
-        __bot.send(randint(1, 10))
+        await __bot.send(randint(1, 10))
 
-def __command_dice(message):
-    __bot.send(f'Cube One: {randint(1, 6)}, Cube Two: {randint(1, 6)}')
+async def __command_dice(message):
+    await __bot.send(f'Cube One: {randint(1, 6)}, Cube Two: {randint(1, 6)}')
 
-def __command_hello(message):
-    __bot.send(f'Hello {message.sender}!')
+async def __command_hello(message):
+    await __bot.send(f'Hello {message.sender}!')
+
+async def __command_checkchannel(message):
+    await __bot.send(f'#{message.channel}')
 
 if __bot.connect():
     print('Bot connected successfully.')
 
     __bot.read_commands(globals())
-    __bot.wait_until_join()
-    __bot.run()
+    __bot.run(startup)
 else:
     print('Something is wrong... Connection failed.')
