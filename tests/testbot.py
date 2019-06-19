58f76510bb0e4e12ca7c6623da7a5568da99ff71
Tom <25082829+truedl@users.noreply.github.com>
2019-06-19 19:19:37 +0300

    testbot.py

--- tests/testbot.py
+++ tests/testbot.py
@@ -15,15 +15,6 @@ async def startup():
     await __bot.wait_until_join()
     await __bot.change_name_color('CadetBlue')
 
-async def __command_roll(message, args:list=None):
-    if args and int(args[0]) < 101:
-        await message.channel.send(randint(1, int(args[0])))
-    else:
-        await message.channel.send(randint(1, 10))
-
-async def __command_dice(message):
-    await message.channel.send(f'Cube One: {randint(1, 6)}, Cube Two: {randint(1, 6)}')
-
 async def __command_hello(message):
     await message.channel.send(f'Hello {message.sender}!')
 
@@ -31,7 +22,6 @@ async def __command_checkchannel(message):
     await message.channel.send(f'#{message.channel}')
 
 async def __command_checkrepr(message):
-    print(message)
     await message.channel.send('Checked.')
 
 if __bot.connect():
